bdad02f5324847e9c7ef14b8ea7db8b7d4920a97
Rohan Maurya <rohanmaurya1695@gmail.com>
2023-06-14 01:44:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@ import React from "react";
 // import "./App.css";
 
 function App() {
-  let jxs1=<h1>hello universe</h1>;
-  let jxs2=<h1>hello galexy</h1>;
-  return true?jxs1:jxs2;
+  let str = "hello world";
+  let pi = 3.14;
+  let active = true;
+  let isUserLoggedIn = true;
 
+  return (
+    <>
+      <h1>hello world</h1>
+      <h1>{str}</h1>
+      <h1>PI:{pi}</h1>
+      {/**if logged in */}
+      {isUserLoggedIn && <h1>welcome to CDAC</h1>}
+      {/**uses of boolean*/}
+      <h1>{active ? "good morning" : "good night"}</h1>
+      {/** MOST USEFUL  */}
+      {active && <h1>Good Morning</h1>}
+      {active ? <h1>Good Morning</h1> : <h1>Good Night</h1>}
+    </>
+  );
 }
 
 export default App;

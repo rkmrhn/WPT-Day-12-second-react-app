cdd46206d89b6751c71158784466123530091e2c
Rohan Maurya <rohanmaurya1695@gmail.com>
2023-06-14 01:49:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from "react";
 // import "./App.css";
 
 function App() {
-  let str = "hello world";
+  let str = "hello";
   let pi = 3.14;
   let active = true;
-  let isUserLoggedIn = true;
+  let user={id:1,name:"rohan",email:"rohan@gmail.com"};
 
-  return (
+  return(
     <>
-      <h1>hello world</h1>
-      <h1>{str}</h1>
-      <h1>PI:{pi}</h1>
-      {/**if logged in */}
-      {isUserLoggedIn && <h1>welcome to CDAC</h1>}
-      {/**uses of boolean*/}
-      <h1>{active ? "good morning" : "good night"}</h1>
-      {/** MOST USEFUL  */}
-      {active && <h1>Good Morning</h1>}
-      {active ? <h1>Good Morning</h1> : <h1>Good Night</h1>}
+    <h1>{str}</h1>
+    <h1>PI:{pi}</h1>
+    {active && <h1>how are you</h1>}
+    <h1>ID:{user.id} NAME:{user.name} EMAIL:{user.email}</h1>
     </>
-  );
+  )
+
+ 
 }
 
 export default App;

3cbeb8eb0df6dac18e37b4547308696063508f7b
Rohan Maurya <rohanmaurya1695@gmail.com>
2023-06-14 01:58:35 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,24 @@ import React from "react";
 // import "./App.css";
 
 function App() {
-  let str = "hello";
+  let id = "id1";
+  let str = "hello world";
   let pi = 3.14;
-  let active = true;
-  let user={id:1,name:"rohan",email:"rohan@gmail.com"};
 
-  return(
+  return (
     <>
-    <h1>{str}</h1>
-    <h1>PI:{pi}</h1>
-    {active && <h1>how are you</h1>}
-    <h1>ID:{user.id} NAME:{user.name} EMAIL:{user.email}</h1>
-    </>
-  )
+      <h1 id="id1">Hello World</h1>
+      <h1 id={id}>{str}</h1>
 
- 
+      {/** USE CASES*/}
+      <h1 id="id1">Hello World</h1>
+      <h1 id={"id1"}>Hello World</h1>
+      <h1 id={`id1`}>Hello World</h1>
+      <h1 id={id}>Hello World</h1>
+
+      <h1>PI {pi} !!</h1>
+    </>
+  );
 }
 
 export default App;
